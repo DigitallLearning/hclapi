57cfdd00cb0fc57c78c11f5db649f0fc125ec4ba
digitlalearning <digitallearning1505@gmail.com>
2024-03-12 12:50:31 +0530

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require("express")
+const cors=require("cors")
 const app=express()
 require("./mongoose")
 app.use(express.json())
+app.use(cors())
 const studentmodel=require("./studentSchema")
 
 app.post("/",async(req,resp)=>{
